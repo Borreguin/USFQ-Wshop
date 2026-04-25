87bbebe4d2aa01d66cd21d7a73271a23856959e0
Jairo Cabrera <jairocabrera87@hotmail.com>
2026-04-25 10:02:40 -0500

    Creando estructura base del proyecto

---
+++ _Soluciones/Grupo1/run.py
@@ -0,0 +1,40 @@
+from Taller1.P1_TSP.TSP import study_case_1
+
+def main():
+    print('Bienvenido al taller 1 de Inteligencia Artificial')
+    print('\nEstudio de casos')
+    print('1. TSP')
+    print('2. N-body')
+    print('3. Juego 2048')
+    print('4. Otro')
+    study_case = input('Ingrese el estudio de caso que desea ejecutar: ')
+    if study_case == '1':
+        print('\n=========================')
+        print('Viaje del vendedor (TSP)')
+        print('=========================')
+        study_case_1()
+        print('\nFin del estudio de caso 1\n')
+        print('=========================')
+        print('Viaje del vendedor (TSP) caso 2')
+        print('=========================')
+        study_case_2()
+        print('\nFin del estudio de caso 2\n')
+        print('=========================')
+    elif study_case == '2':
+        print('\n=========================')
+        print('N-body')
+        print('=========================')
+        study_case_2()
+        print('\nFin del estudio de caso 2\n')
+    elif study_case == '3':
+        print('\n=========================')
+        print('Juego 2048')
+        print('=========================')
+        study_case_3()
+        print('\nFin del estudio de caso 3\n')
+        print('=========================')
+    else:
+        print('No hay el texto')
+
+if __name__ == "__main__":
+    main()
