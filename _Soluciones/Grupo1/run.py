21f58f46e1c4eae193bf76c8512ccba6df08ba7a
Jairo Cabrera <jairocabrera87@hotmail.com>
2026-05-06 22:05:40 -0500

    Updating files to run all taller types

--- _Soluciones/Grupo1/run.py
+++ _Soluciones/Grupo1/run.py
@@ -1,8 +1,9 @@
 from Taller1.P1_TSP.TSP import study_case_1, study_case_2
 from Taller1.P2_Granjero.Acertijo import main as granjero_main
 from Taller1.P3_Torres.Torres import main as torres_main
+from Taller2.P2.P2_ACO import study_case_1 as aco_study_case_1, study_case_2 as aco_study_case_2
 
-def main():
+def taller_1():
     print('Bienvenido al taller 1 de Inteligencia Artificial')
     print('\nEstudio de casos')
     print('1. TSP')
@@ -10,33 +11,81 @@ def main():
     print('3. Juego 2048')
     print('4. Otro')
     study_case = input('Ingrese el estudio de caso que desea ejecutar: ')
-    if study_case == '1':
-        print('\n=========================')
-        print('Viaje del vendedor (TSP)')
-        print('=========================')
-        study_case_1()
-        print('\nFin del estudio de caso 1\n')
-        print('=========================')
-        print('Viaje del vendedor (TSP) caso 2')
-        print('=========================')
-        study_case_2()
-        print('\nFin del estudio de caso 2\n')
-        print('=========================')
-    elif study_case == '2':
-        print('\n=========================')
-        print('Acertijo del granjero y el bote')
-        print('=========================')
-        granjero_main()
-        print('\nFin del estudio de caso 2\n')
-    elif study_case == '3':
-        print('\n=========================')
-        print('Juego 2048')
-        print('=========================')
-        torres_main()
-        print('\nFin del estudio de caso 3\n')
-        print('=========================')
-    else:
-        print('No hay el texto')
+    while study_case != '5':
+        if study_case == '1':
+            print('\n=========================')
+            print('Viaje del vendedor (TSP)')
+            print('=========================')
+            study_case_1()
+            print('\nFin del estudio de caso 1\n')
+            print('=========================')
+            print('Viaje del vendedor (TSP) caso 2')
+            print('=========================')
+            study_case_2()
+            print('\nFin del estudio de caso 2\n')
+            print('=========================')
+        elif study_case == '2':
+            print('\n=========================')
+            print('Acertijo del granjero y el bote')
+            print('=========================')
+            granjero_main()
+            print('\nFin del estudio de caso 2\n')
+        elif study_case == '3':
+            print('\n=========================')
+            print('Torres de Hanoi')
+            print('=========================')
+            torres_main()
+            print('\nFin del estudio de caso 3\n')
+            print('=========================')
+        else :
+            print('No hay el texto')
+        study_case = input('Ingrese el estudio de caso que desea ejecutar: ')
+    print('Saliendo del taller 1')
+
+def taller_2():
+    print('Bienvenido al taller 2 de Inteligencia Artificial')
+    print('\nEstudio de casos')
+    print('1. Laberinto')
+    print('2. Colonia de Hormigas')
+    print('3. Otro')
+    print('4. Salir')
+    study_case = input('Ingrese el estudio de caso que desea ejecutar: ')
+    while study_case != '4':
+        if study_case == '1':
+            print('\n=========================')
+            print('Llamado al laberinto ')
+            print('=========================')
+        elif study_case == '2':
+            print('\n=========================')
+            print('Colonia de Hormigas')
+            print('=========================')
+            aco_study_case_1()
+            print('\nFin del estudio de caso 1\n')
+            print('=========================')
+            print('Colonia de Hormigas caso 2')
+            print('=========================')
+            aco_study_case_2()
+            print('\nFin del estudio de caso 2\n')
+        else :
+            print('No hay el texto')
+        study_case = input('Ingrese el estudio de caso que desea ejecutar: ')
+    print('Saliendo del taller 2')
+
+def main():
+    print('Bienvenido al taller de Inteligencia Artificial')
+    print('1. Taller 1')
+    print('2. Taller 2')
+    print('3. Salir')
+    option = input('Ingrese el taller que desea ejecutar: ')
+    while option != '3':
+        if option == '1':
+            taller_1()
+        elif option == '2':
+            taller_2()
+        else:
+            print('No hay el texto')
+        option = input('Ingrese el taller que desea ejecutar: ')
+    print('Fin del programa')
 
 if __name__ == "__main__":
     main()
